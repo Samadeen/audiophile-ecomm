a867700d0a7eb53a97d772b43da117a44db05357
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-07-19 22:31:47 +0100

    fix

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -87,7 +87,7 @@ const Product: React.FC = () => {
       <section className={styles.product_container}>
         <h6 onClick={back}>Go Back</h6>
         <div className={styles.product_details}>
-          <img src={`./src/${productDesktopImage}`} alt={product.name} />
+          <img src={`${productDesktopImage}`} alt={product.name} />
           <div className={styles.productCard_details}>
             {product.new && <h5>New Product</h5>}
             <h2>{product.name}</h2>
@@ -140,21 +140,21 @@ const Product: React.FC = () => {
         </div>
         <div className={styles.product_gallery}>
           <div className={styles.image_small}>
-            <img src={`src/${galleryFirstDesktopImage}`} alt={product.name} />
-            <img src={`src/${gallerySecondDesktopImage}`} alt={product.name} />
+            <img src={`${galleryFirstDesktopImage}`} alt={product.name} />
+            <img src={`${gallerySecondDesktopImage}`} alt={product.name} />
           </div>
-          <img src={`src/${galleryThirdDesktopImage}`} alt={product.name} />
+          <img src={`${galleryThirdDesktopImage}`} alt={product.name} />
         </div>
         <div className={styles.product_alike}>
           <h2>You may also like</h2>
           <div className={styles.boxes}>
             <div className={styles.box}>
-              <img src={`src/${imagesYouMayLikeOne}`} alt={product.name} />
+              <img src={`/${imagesYouMayLikeOne}`} alt={product.name} />
               <h3>{product.others[0]?.name}</h3>
               <Button
                 btnText='See Product'
                 orange={true}
-                product={`/${
+                product={`${
                   product.others[0]?.slug === 'xx99-mark-one-headphones'
                     ? 3
                     : product.others[0]?.slug === 'xx99-mark-two-headphones'
@@ -173,12 +173,12 @@ const Product: React.FC = () => {
               />
             </div>
             <div className={styles.box}>
-              <img src={`src/${imagesYouMayLikeTwo}`} alt={product.name} />
+              <img src={`/${imagesYouMayLikeTwo}`} alt={product.name} />
               <h3>{product.others[1]?.name}</h3>
               <Button
                 btnText='See Product'
                 orange={true}
-                product={`/${
+                product={`${
                   product.others[1]?.slug === 'xx99-mark-one-headphones'
                     ? 3
                     : product.others[1]?.slug === 'xx99-mark-two-headphones'
@@ -197,12 +197,12 @@ const Product: React.FC = () => {
               />
             </div>
             <div className={styles.box}>
-              <img src={`src/${imagesYouMayLikeThree}`} alt={product.name} />
+              <img src={`/${imagesYouMayLikeThree}`} alt={product.name} />
               <h3>{product.others[2]?.name}</h3>
               <Button
                 btnText='See Product'
                 orange={true}
-                product={`/${
+                product={`${
                   product.others[2]?.slug === 'xx99-mark-one-headphones'
                     ? 3
                     : product.others[2]?.slug === 'xx99-mark-two-headphones'
